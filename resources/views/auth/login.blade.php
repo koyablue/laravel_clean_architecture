0771127907ee24b3743aeb1bcc778ba4b2df3bec
koya_aoyama <aoyama.ko0920@gmail.com>
2020-09-27 17:16:16 +0900

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,31 @@
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <div class="form-group row">
-        <label for="email">E-Mail Address</label>
-        <input id="email" type="email" name="email" value="{{ old('email') }}">
-
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-    </div>
-
-    <div class="form-group row">
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password">
-
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-    </div>
-
-    <div class="form-group row mb-0">
-        <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                Login
-            </button>
+@extends('layouts.base_layout')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">E-Mail Address</span>
+                                </div>
+                                <input class="form-control" type="email" name="email" value="{{ old('email') }}">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Password</span>
+                                </div>
+                                <input class="form-control" type="password" name="password" value="{{ old('password') }}">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</form>
-
+@endsection
 
